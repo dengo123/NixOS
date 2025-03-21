780f688543c09193e99decb9a76dcc816f269a00
dengo123 <deniz060198@hotmail.com>
2025-03-21 16:18:53 +0100

    commit zen-browser

--- flake.nix
+++ flake.nix
@@ -14,109 +14,98 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
-    
+
     ghostty.url = "github:ghostty-org/ghostty";
-
     cosmic.url = "github:lilyinstarlight/nixos-cosmic";
-    
     stylix.url = "github:danth/stylix";
-
     nixvim.url = "github:nix-community/nixvim";
+    zen-browser.url = "github:MarceColl/zen-browser-flake";
   };
 
-  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager-stable, home-manager-unstable, cosmic, ghostty, stylix, nixvim, ... }:
-    let
-      system = "x86_64-linux";
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager-stable, home-manager-unstable, cosmic, ghostty, stylix, nixvim, zen-browser, ... }:
+  let
+    zenOverlay = final: prev: {
+      zen-browser = zen-browser.packages.${final.system}.default;
+    };
 
-      # ---- SYSTEM SETTINGS ---- #
-      systemSettings = {
-        system = system;
-        hostname = "main";
-        profile = "main";  # Dynamische Profilwahl
-        timezone = "Europe/Berlin";
-        locale = "de_DE.UTF-8";
-        grubDevice = "";
-        gpuType = "nvidia";
-      };
+    system = "x86_64-linux";
 
-      # ----- USER SETTINGS ----- #
-      userSettings = rec {
-        username = "dengo123";
-        name = "Deniz";
-        email = "deniz060198@hotmail.com";
-        dotfilesDir = "~/.dotfiles";
-        theme = "everforest";
-        wm = "cosmic";
-        browser = "";
-        term = "ghostty";
-        shell = "zsh";
-        font = "Intel One Mono";
-        fontPkg = pkgs.intel-one-mono;
-        editor = "neovim";
-      };
+    # ---- SYSTEM SETTINGS ---- #
+    systemSettings = {
+      system = system;
+      hostname = "main";
+      profile = "main";  # Dynamische Profilwahl
+      timezone = "Europe/Berlin";
+      locale = "de_DE.UTF-8";
+      grubDevice = "";
+      gpuType = "nvidia";
+    };
 
-      # ----- PACKAGES ----- #
-      overlays = [ cosmic.overlay ];
-      lib = nixpkgs.lib;
+    # ----- USER SETTINGS ----- #
+    userSettings = rec {
+      username = "dengo123";
+      name = "Deniz";
+      email = "deniz060198@hotmail.com";
+      dotfilesDir = "~/.dotfiles";
+      theme = "everforest";
+      wm = "cosmic";
+      browser = "";
+      term = "ghostty";
+      shell = "zsh";
+      font = "Intel One Mono";
+      fontPkg = pkgs.intel-one-mono;
+      editor = "neovim";
+    };
 
-      # Definiere stable und unstable separat
-      pkgs-stable = import inputs.nixpkgs {
-        system = systemSettings.system;
-        config = {
-          allowUnfree = true;
-          overlays = overlays;
-        };
-      };
+    # ----- PACKAGES ----- #
+    overlays = [ cosmic.overlay zenOverlay ];
+    lib = nixpkgs.lib;
 
-      pkgs-unstable = import inputs.nixpkgs-unstable {
-        system = systemSettings.system;
-        config = {
-          allowUnfree = true;
-          overlays = overlays;
-        };
-      };
-      
-      pkgs = ( if systemSettings.profile == "main" then pkgs-unstable else pkgs-stable);
-
-      # Dynamische Auswahl basierend auf `systemSettings.profile`
-      useUnstable = systemSettings.profile == "main";
-
-      # Automatische Auswahl von Home Manager (stable oder unstable)
-      home-manager = if useUnstable then home-manager-unstable else home-manager-stable;
-
-      specialArgs = if userSettings.wm == "cosmic" then
-        { inherit systemSettings userSettings inputs; }
-      else
-        { inherit systemSettings userSettings inputs pkgs; };
-
-    in {
-      nixosConfigurations = {
-        system = lib.nixosSystem {
-          system = systemSettings.system;
-          modules = [
-            ./profiles/${systemSettings.profile}/configuration.nix
-            {
-              nix.settings = {
-                substituters = [ "https://cosmic.cachix.org/" ];
-                trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
-              };
-            }
-            cosmic.nixosModules.default
-          ];
-          specialArgs = specialArgs;
-        };
-      };
-
-      homeConfigurations = {
-        user = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [
-            ./profiles/${systemSettings.profile}/home.nix
-	    nixvim.homeManagerModules.nixvim
-          ];
-          extraSpecialArgs = specialArgs;
-        };
+    # Definiere stable und unstable separat
+    pkgs-stable = import inputs.nixpkgs {
+      system = systemSettings.system;
+      config = {
+        allowUnfree = true;
+        overlays = overlays;
       };
     };
+
+    pkgs-unstable = import inputs.nixpkgs-unstable {
+      system = systemSettings.system;
+      config = {
+        allowUnfree = true;
+        overlays = overlays;
+      };
+    };
+
+  in {
+    nixosConfigurations = {
+      system = lib.nixosSystem {
+        system = systemSettings.system;
+        modules = [
+          ./profiles/${systemSettings.profile}/configuration.nix
+          {
+            nix.settings = {
+              substituters = [ "https://cosmic.cachix.org/" ];
+              trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+            };
+          }
+          cosmic.nixosModules.default
+        ];
+        specialArgs = specialArgs;
+      };
+    };
+
+    homeConfigurations = {
+      user = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          ./profiles/${systemSettingspprofile}/home.nix
+          nixvim.homeManagerModules.nixvim
+        ];
+        extraSpecialArgs = specialArgs;
+      };
+    };
+  };
 }
 
