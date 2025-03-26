5806975f8ae73b5db60c68de50a38f201be6f562
dengo123 <deniz060198@hotmail.com>
2025-03-26 12:53:25 +0100

    commit omp changes final

--- user/shell/zsh/plugins/omp.nix
+++ user/shell/zsh/plugins/omp.nix
@@ -1,35 +1,27 @@
 { config, lib, pkgs, ... }:
 
 let
-  # dein Prompt-Layout ohne palette
-  templateBase = ../omp/template-base16.toml;
-  renderedPath = "${config.xdg.configHome}/oh-my-posh/themes/generated.omp.toml";
+  templatePath = ../omp/template.toml;
+  outputPath = "${config.xdg.configHome}/oh-my-posh/themes/generated.omp.toml";
 in {
   programs.oh-my-posh = {
     enable = true;
     enableZshIntegration = true;
   };
 
-  # dynamische Farben bei Aktivierung einfügen
   home.activation.generateOMP = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    TEMPLATE="${templateBase}"
-    OUT="${renderedPath}"
+    TEMPLATE="${templatePath}"
+    OUT="${outputPath}"
 
     mkdir -p "$(dirname "$OUT")"
 
     if [ -f /etc/stylix/colors.toml ]; then
-      echo "[OMP] Generating Stylix base16 palette into OMP template..."
+      echo "[OMP] Generating prompt from Stylix colors..."
 
-      {
-        echo 'version = 3'
-        echo 'final_space = true'
-        echo ''
-        echo '[palette]'
-        grep -E '^base[0-9A-Fa-f]+ *= *' /etc/stylix/colors.toml \
-          | sed -E 's/ *= */ = "/; s/"//g; s/$/"/'
-        echo ''
-        cat "$TEMPLATE"
-      } > "$OUT"
+      export $(grep -E '^base[0-9A-F]+ *= *' /etc/stylix/colors.toml \
+        | sed -E 's/"//g; s/ *= */=#/g')
+
+      sed -E 's/\{\{([a-zA-Z0-9]+)\}\}/\$\1/g' "$TEMPLATE" | envsubst > "$OUT"
     else
       echo "[OMP] Warning: /etc/stylix/colors.toml not found – skipping generation." >&2
     fi
