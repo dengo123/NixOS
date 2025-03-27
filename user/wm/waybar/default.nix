9ca9624591b2bbe427c15588f33e6da07bfb732c
dengo123 <deniz060198@hotmail.com>
2025-03-27 02:27:02 +0100

    rm style.css waybar

--- user/wm/waybar/default.nix
+++ user/wm/waybar/default.nix
@@ -1,21 +1,17 @@
-{ config, pkgs, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 let
   waybarConfig = ./config.json;
-  waybarStyle  = ./style.css;
-in {
+in
+{
   programs.waybar = {
     enable = true;
-
     settings = {
       mainBar = builtins.fromJSON (builtins.readFile waybarConfig);
     };
-
-    style = builtins.readFile waybarStyle;
   };
 
   xdg.configFile."waybar/config.json".source = waybarConfig;
-  xdg.configFile."waybar/style.css".source = waybarStyle;
 
   home.packages = with pkgs; [
     waybar
