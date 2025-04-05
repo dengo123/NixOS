50241871d864355a74c3e3162b180c198a01a233
dengo123 <deniz060198@hotmail.com>
2025-04-05 15:31:30 +0200

    changed waybar layout

--- user/wm/hyprland/waybar/modules.nix
+++ user/wm/hyprland/waybar/modules.nix
@@ -3,32 +3,20 @@
 {
   layer = "top";
   position = "top";
-
-  # Waybar verlangt hier einen Integer
-  # Achtung: Falls dein CSS die Leiste höher macht,
-  # bekommst du eine Warnung, "Requested height is less than min. needed".
   height = 35;
-
-  # margin muss bei vielen Waybar-Modulen ein String sein (falls Home-Manager das so fordert).
   margin = "10px 5px 0 5px";
   spacing = 3;
 
-  # Hier die Anordnung der Module:
   "modules-left" = [
     "custom/launcher"
-    "custom/browser"
-    "custom/calculator"
-    "custom/mail"
-    "wlr/taskbar"
-    "hyprland/window" 
-  ];
-
-  "modules-center" = [
     "hyprland/workspaces"
   ];
 
-  "modules-right" = [
+  "modules-center" = [
     "custom/calendar"
+  ];
+
+  "modules-right" = [
     "network"
     "bluetooth"
     "pulseaudio"
@@ -39,45 +27,17 @@
     "custom/power"
   ];
 
-  ########################
-  # Definition der Custom-Module
-  ########################
-
   "custom/launcher" = {
-    # Standard: Du kannst "exec" + "interval" verwenden oder "on-click", "format" usw.
-    # "interval=0" => Waybar führt "exec" nur einmal aus oder auf Klick (je nach Version).
-    # Wenn du nur ein Icon zeigen willst, kannst du `format = ""` verwenden.
     format = "󱄅";
     tooltip = true;
     "on-click" = "rofi -show drun";
     interval = 0;
   };
 
-  "custom/browser" = {
-    format = "";
-    tooltip = "Firefox";
-    "on-click" = "firefox";
-    interval = 0;
-  };
-
-  "custom/calculator" = {
-    format = "󰪚";
-    tooltip = "Calculator";
-    "on-click" = "gnome-calculator";
-    interval = 0;
-  };
-
-  "custom/mail" = {
-    format = "";
-    tooltip = "E-Mail (Thunderbird)";
-    "on-click" = "thunderbird";
-    interval = 0;
-  };
-
   "custom/calendar" = {
     "exec" = "date +'%a %d.%m %H:%M'";
     "interval" = 60;
-    "format" = "{}"; 
+    "format" = "{}";
     "on-click" = "gsimplecal";
   };
 
@@ -97,10 +57,6 @@
     tooltip = "Power Menu";
     "on-click" = "wlogout";
   };
-
-  ########################
-  # Definition der eingebauten Module
-  ########################
 
   "pulseaudio" = {
     format = "{icon} {volume}%";
